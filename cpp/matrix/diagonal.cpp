d901cce12c0016559a205dddca0ec5294431b8bc
Rajat Roy <rajatroy.dev@gmail.com>
2021-12-21 19:41:28 +0530

    C++: Minor changes

--- cpp/matrix/diagonal.cpp
+++ cpp/matrix/diagonal.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-// Diagonal matrix is a square matrix whose diagonal elements are non-zero rest all elements are zero.
-class Matrix
+// Diagonal diagonal is a square diagonal whose diagonal elements are non-zero rest all elements are zero.
+class Diagonal
 {
     int *array;
     int array_size;
 
 public:
-    Matrix(int array_size)
+    Diagonal(int array_size)
     {
         this->array_size = array_size;
         this->array = new int[array_size];
+    }
+
+    ~Diagonal()
+    {
+        delete[] this->array;
     }
 
     void set(int m, int n, int x)
@@ -46,28 +51,28 @@ public:
 
 int main()
 {
-    Matrix *matrix = new Matrix(3);
+    Diagonal *diagonal = new Diagonal(3);
 
-    matrix->set(1, 1, 1);
-    matrix->set(1, 2, 2);
-    matrix->set(1, 3, 3);
-    matrix->set(2, 1, 1);
-    matrix->set(2, 2, 2);
-    matrix->set(2, 3, 3);
-    matrix->set(3, 1, 1);
-    matrix->set(3, 2, 2);
-    matrix->set(3, 3, 3);
+    diagonal->set(1, 1, 1);
+    diagonal->set(1, 2, 2);
+    diagonal->set(1, 3, 3);
+    diagonal->set(2, 1, 1);
+    diagonal->set(2, 2, 2);
+    diagonal->set(2, 3, 3);
+    diagonal->set(3, 1, 1);
+    diagonal->set(3, 2, 2);
+    diagonal->set(3, 3, 3);
 
-    cout << "(1, 1): " << matrix->get(1, 1) << endl;
-    cout << "(1, 2): " << matrix->get(1, 2) << endl;
-    cout << "(1, 3): " << matrix->get(1, 3) << endl;
-    cout << "(2, 1): " << matrix->get(2, 1) << endl;
-    cout << "(2, 2): " << matrix->get(2, 2) << endl;
-    cout << "(2, 3): " << matrix->get(2, 3) << endl;
-    cout << "(3, 1): " << matrix->get(3, 1) << endl;
-    cout << "(3, 2): " << matrix->get(3, 2) << endl;
-    cout << "(3, 3): " << matrix->get(3, 3) << endl;
+    cout << "(1, 1): " << diagonal->get(1, 1) << endl;
+    cout << "(1, 2): " << diagonal->get(1, 2) << endl;
+    cout << "(1, 3): " << diagonal->get(1, 3) << endl;
+    cout << "(2, 1): " << diagonal->get(2, 1) << endl;
+    cout << "(2, 2): " << diagonal->get(2, 2) << endl;
+    cout << "(2, 3): " << diagonal->get(2, 3) << endl;
+    cout << "(3, 1): " << diagonal->get(3, 1) << endl;
+    cout << "(3, 2): " << diagonal->get(3, 2) << endl;
+    cout << "(3, 3): " << diagonal->get(3, 3) << endl;
 
-    matrix->display();
+    diagonal->display();
     return 0;
 }
