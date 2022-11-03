f94b26be7d1dd32fc12e9a6f25a7a9cc1b0c66a0
Rajat Roy <rajatroy.dev@gmail.com>
2022-11-03 21:39:36 +0530

    Leetcode - easy - sortedFrequencyOfStrings

---
+++ leetcode/easy/SortedFrequencyOfStrings.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedFrequencyOfStrings {
+    static void frequency(List<String> list) {
+        SortedMap<String, Integer> map = new TreeMap<>();
+
+        for (String string : list) {
+            String key = string.trim();
+            if (key.length() > 0) {
+                if (map.containsKey(key)) {
+                    int value = map.get(key);
+                    map.put(key, ++value);
+                } else {
+                    map.put(key, 1);
+                }
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("A");
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("D");
+        list.add("D");
+
+        frequency(list);
+    }
+}
